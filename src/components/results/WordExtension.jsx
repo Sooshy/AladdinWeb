3062b9d6fa0f777a5739ba45e4a5cfb8fa25096e
Iris <iris.p.98@gmail.com>
2020-04-23 10:23:17 +0300

    added edit word before export to wordExtension- not functional

--- src/components/results/WordExtension.jsx
+++ src/components/results/WordExtension.jsx
@@ -7,7 +7,9 @@ import { extensionColors } from '../../design/viewSettings';
 import { bindActionCreators } from "redux";
 import { addWordExtensionsToExport, deleteWordExtensionFromExport } from "../../redux/actions";
 import { connect } from "react-redux";
-
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DoneOutlinedIcon from '@material-ui/icons/DoneOutlined';
+import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({
     margin: {
@@ -15,10 +17,18 @@ const styles = theme => ({
     },
     extensionColor: {
         backgroundColor: (props) => extensionColors[props.extensionType]
+    },
+    textArea: {
+        width: theme.spacing(10)
     }
 });
 
 class WordExtension extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { editMode: false };
+    };
 
     isWordMarkedForExport = () => {
         if (this.props.extensionsByWordToExport.hasOwnProperty(this.props.wordToExtend)) {
@@ -38,16 +48,20 @@ class WordExtension extends React.Component {
         const { classes } = this.props;
 
         const baseWordView = <Badge classes={{ badge: classes.extensionColor }} className={classes.margin} variant="dot"><Typography variant="subtitle1">{this.props.word}</Typography></Badge>;
-
+        const editWordView = <TextField  className={classes.textArea} defaultValue={this.props.word} />
         if (!this.props.exportMode) {
             return baseWordView;
-        }
+        };
 
         if (this.isWordMarkedForExport()) {
             return <Chip onClick={() => this.props.deleteWordFromExport({ word: this.props.wordToExtend, extensionType: this.props.extensionType, wordExtension: this.props.word })} className={classes.margin} color="primary" label={baseWordView}></Chip>
-        }
+        };
 
-        return <Chip onClick={() => this.props.addWordsToExport([{ word: this.props.wordToExtend, extensionType: this.props.extensionType, wordExtension: this.props.word }])} className={classes.margin} label={baseWordView}></Chip>
+        if (this.state.editMode) {
+            return <Chip deleteIcon={<DoneOutlinedIcon />} onDelete={() => { this.setState({ editMode: false }) }} className={`${classes.margin}`} label={editWordView}></Chip>
+        };
+
+        return <Chip deleteIcon={<EditOutlinedIcon />} onDelete={() => { this.setState({ editMode: true }) }} onClick={() => this.props.addWordsToExport([{ word: this.props.wordToExtend, extensionType: this.props.extensionType, wordExtension: this.props.word }])} className={classes.margin} label={baseWordView}></Chip>
     }
 }
 
